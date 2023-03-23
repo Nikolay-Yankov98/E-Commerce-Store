f9474f54480e175c06b065be6bf4e651e26ec154
Nikolay <n.qnkov98@gmail.com>
2023-03-23 23:41:59 +0200

    Add project files.

---
+++ E-Commerce Store/Program.cs
@@ -0,0 +1,34 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+//service for the DB
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    //the Connection string for the given DB
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConneciton"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
