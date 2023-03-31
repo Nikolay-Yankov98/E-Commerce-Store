eee844f8cce08d6021569140b2098f4060fae2d6
Nikolay <n.qnkov98@gmail.com>
2023-03-31 05:57:41 +0300

    Started implementing generic repo

--- E-Commerce Store/Program.cs
+++ E-Commerce Store/Program.cs
@@ -17,6 +17,8 @@ builder.Services.AddDbContext<StoreContext>(opt =>
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConneciton"));
 });
 builder.Services.AddScoped<IProductRepository,ProductRepository>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
